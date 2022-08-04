b5f8e26460181cf6caf1d6e8502d1ec1679518d8
Gabriel <sgabrielok01@gmail.com>
2022-08-03 23:31:34 -0300

    arreglos deploy

--- api/index.js
+++ api/index.js
@@ -19,7 +19,6 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-require("dotenv").config();
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
