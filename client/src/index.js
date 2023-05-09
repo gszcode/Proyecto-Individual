393d4999feb99aeb0bd8655211b3ba8000b197d4
Gabriel <gszcode@gmail.com>
2023-05-09 20:08:29 -0300

    redeploy

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./redux/store";
-import axios from "axios";
-import dotenv from "dotenv";
-dotenv.config();
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './redux/store'
+import axios from 'axios'
+import dotenv from 'dotenv'
+dotenv.config()
 
-axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
+axios.defaults.baseURL =
+  process.env.REACT_APP_API ||
+  'http://localhost:3001' ||
+  'https://proyecto-individual-production-b3c9.up.railway.app/'
 
 ReactDOM.render(
   <Provider store={store}>
@@ -17,5 +20,5 @@ ReactDOM.render(
       <App />
     </BrowserRouter>
   </Provider>,
-  document.getElementById("root")
-);
+  document.getElementById('root')
+)
